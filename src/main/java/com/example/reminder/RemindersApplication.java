fedcc077a552b725c1c78d7d79e6a3a5445367b7
vall0204 <vall020404@yandex.ru>
2025-08-27 12:15:12 +0300

    first commit

---
+++ src/main/java/com/example/reminder/RemindersApplication.java
@@ -0,0 +1,13 @@
+package com.example.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemindersApplication.class, args);
+    }
+
+}
